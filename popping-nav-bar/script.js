378973675b7f79967f3d871a43017e1391c06f7d
Marius Cozma <mariuscozmabe@gmail.com>
2025-06-18 16:52:01 +0300

    menubar logic

--- popping-nav-bar/script.js
+++ popping-nav-bar/script.js
@@ -1,9 +1,15 @@
 const navbar = document.querySelector(".nav");
+const mql = window.matchMedia("(max-width: 900px)");
 
-window.addEventListener("scroll", ()=>{
-    if(this.scrollY>1){
-        navbar.classList.add("slidedown")
-    }else {
-        navbar.classList.remove("slidedown")
+window.addEventListener("scroll", () => {
+  if (!mql.matches) {
+    if (window.scrollY > 1) {
+      navbar.classList.add("slidedown");
+    } else {
+      navbar.classList.remove("slidedown");
     }
-})
+  } else {
+    navbar.classList.remove("slidedown");
+  }
+});
+
